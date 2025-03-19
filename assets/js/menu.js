0790c045a1f4cb2472e1e1ac666e495340dd5889
ANONYMOUS <146953354+Mateojimenezz@users.noreply.github.com>
2025-03-18 19:57:44 -0500

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".navbar-toggler");
+    const logo = document.querySelector(".logo");
+    const menu = document.getElementById("menu");
+  
+    menuToggle.addEventListener("click", function () {
+      const isMenuOpen = menu.classList.contains("show");
+  
+      if (isMenuOpen) {
+        // Cuando el menú está abierto, restauramos el logo
+        logo.style.display = "flex";
+      } else {
+        // Cuando el menú está cerrado, ocultamos el logo
+        logo.style.display = "none";
+      }
+    });
+  
+    // Detectar cuando el usuario cierra el menú automáticamente
+    menu.addEventListener("hidden.bs.collapse", function () {
+      logo.style.display = "flex";
+    });
+  });
+  
+  
+  
+  
+  
